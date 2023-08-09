8124c906f7f59ea699948231b03fff50a0b3e689
Sridivya321 <sridivya.bunga@infolob.com>
2023-08-09 10:40:54 +0530

    adding environment variables

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 import os
+import glob
 from dask import dataframe as dd
 
 
 def main():
     print('file format conversion started')
     src_dir=os.environ['SRC_DIR']
-    tgt_dir=os.environ['TGT_DIR']
+    # tgt_dir=os.environ['TGT_DIR']
+    src_file_pattern=os.environ.setdefault('SRC_FILE_PATTERN','NYSE*txt.gz')
+    src_file_names=sorted(glob.glob(f'{src_dir}/{src_file_pattern}'))
+    tgt_file_names=[
+        file.replace('txt','json').replace('nyse_data','nyse_json')
+        for file in src_file_names
+    ]
 
-    df=dd.read_csv(f'{src_dir}/NYSE*.txt.gz',
+    df=dd.read_csv(src_file_names,
                    names=['ticker','trade_date','open_price','low_price','high_price','close_price','volume'],
                           blocksize=None)
     print('dataframe is created and will write json format')
 
-    df.to_json(f'{tgt_dir}/part*.json.gz',
+    df.to_json(tgt_file_names,
                orient='records',
                lines=True,
                compression='gzip',
-               name_function=lambda i:'%05d' %i)
+            #    name_function=lambda i:'%05d' %i
+            )
     
     print('processing files completed')
 if __name__=="__main__":
